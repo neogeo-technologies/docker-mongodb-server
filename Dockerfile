61d6ae673e86fce7f015b3dc07c64240803f6515
bchartier <benjamin.chartier@cegetel.net>
2016-02-11 17:08:30 +0100

    Add admin user

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,22 @@ MAINTAINER Neogeo Technologies http://www.neogeo-online.net
 ADD mongo.repo /etc/yum.repos.d/mongodb-org-3.2.repo
 RUN yum -y update; yum clean all
 RUN yum -y install epel-release; yum clean all
-RUN yum -y install mongodb-org; yum clean all
-RUN mkdir -p /data/db
-RUN mkdir -p /data/log
+RUN yum -y install \
+	mongodb-org \
+	pwgen \
+	; yum clean all
+
+ENV DATADIR /data
+ENV SUPERUSER neogeo
+# ENV SUPERPASS neogeo
+
 ADD ./mongod.conf /etc/mongod.conf
 
-VOLUME ["/data/db"]
-VOLUME ["/data/log"]
+# Add scripts
+ADD scripts /scripts
+RUN chmod +x /scripts/*.sh
 
+CMD ["/scripts/run.sh"]
+
+VOLUME ["$DATADIR/db", "$DATADIR/log"]
 EXPOSE 27017
-
-ENTRYPOINT ["/usr/bin/mongod", "--config", "/etc/mongod.conf"]
---
+++ scripts/run.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#enable job control in script
+set -m
+
+DATAPATH=${DATADIR:-/data}
+mkdir -p $DATADIR/db
+mkdir -p $DATADIR/log
+
+# Configure MongoDB
+if [ ! -f /data/db/.mongo_admin_created ]; then
+
+	PASS=${SUPERPASS:-$(pwgen -s -1 16)}
+
+	sed -i "s|dbPath: /data/db|dbPath: $DATAPATH/db|" /etc/mongod.conf
+	sed -i "s|path: /data/log/mongod.log|path: $DATAPATH/log/mongod.log|" /etc/mongod.conf
+fi
+
+# Run MongoDB in background
+/usr/bin/mongod --auth --config /etc/mongod.conf &
+
+# Create admin user
+if [ ! -f /data/db/.mongo_admin_created ]; then
+
+	# If the superuser password is not defnied in an environment variable
+	# it is randomly generated
+
+	echo "SUPERUSER: \"$SUPERUSER\""
+	echo "SUPERPASS: \"$PASS\""
+ 	echo "DATADIR: \"$DATAPATH\""
+
+	RET=1
+	while [[ RET -ne 0 ]]; do
+	    echo "=> Waiting for confirmation of MongoDB service startup"
+	    sleep 5
+    	mongo admin --eval "help" >/dev/null 2>&1
+	    RET=$?
+	    echo RET
+	done
+
+	mongo admin --eval "db.createUser({user: '$SUPERUSER', pwd: '$PASS', roles:[{role:'root',db:'admin'}]});"
+	touch /data/db/.mongo_admin_created
+
+	echo "========================================================================"
+	echo "You can now connect to this MongoDB server using:"
+	echo "    mongo admin -u $SUPERUSER -p $PASS --host <host> --port <port>"
+	echo "========================================================================"
+fi
+
+# Bring MongoDB to foreground
+fg
